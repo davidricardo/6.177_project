d5aef0ecbb255650e5366563ee9573acaa933583
davidricardo <davidr.ricardo@gmail.com>
2015-01-16 14:13:38 -0500

    Set up empty fixtures file
    This file can be used to give models initial data in JSON format.

--- oursite/chargen/models.py
+++ oursite/chargen/models.py
@@ -1,9 +1,9 @@
 """
 This is where models, which are tables, will be created.
 
-See https://docs.djangoproject.com/en/1.6/intro/tutorial01/
-and https://docs.djangoproject.com/en/1.6/ref/models/instances/#django.db.models.Model
-for more information.
+See https://docs.djangoproject.com/en/1.6/intro/tutorial01/ , 
+https://docs.djangoproject.com/en/1.6/ref/models/instances/#django.db.models.Model , or
+https://docs.djangoproject.com/en/1.6/howto/custom-model-fields/ for more information.
 """
 
 from django.db import models
