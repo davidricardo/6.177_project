5ea5b1e0f7640e1a420d82760bb4d64d3c799734
RachaelDevlin <rdevlin@mit.edu>
2015-01-16 22:27:28 -0500

    database

--- oursite/chargen/admin.py
+++ oursite/chargen/admin.py
@@ -4,5 +4,7 @@ We can use the admin verson to make data entry easier.
 """
 
 from django.contrib import admin
+from chargen.models import Weapon
+admin.site.register(Weapon)
 
 # Register your models here.
--- oursite/chargen/models.py
+++ oursite/chargen/models.py
@@ -9,3 +9,31 @@ https://docs.djangoproject.com/en/1.6/howto/custom-model-fields/ for more inform
 from django.db import models
 
 # Create your models here.
+
+class Weapon(models.Model):
+    weapon_name= models.CharField(max_length=30)
+    martial_arts= models.BooleanField(default=False)
+    mele= models.BooleanField(default=True)
+    finesse= models.BooleanField(default=False)
+    damage_type= models.CharField(max_length=20)
+    two_handed= models.BooleanField(default=False)
+    number_of_damage_die= models.IntegerField()
+    type_of_damage_die= models.IntegerField()
+    range_close= models.IntegerField(default=0, blank=True)
+    range_max=models.IntegerField(default=0,blank=True)
+    
+    
+    
+    
+    def __unicode__(self):
+        return self.weapon_name
+class Race(models.Model):
+    name= models.CharField(max_length=25)
+    dex_mod= models.IntegerField(default=0)
+    char_mod= models.IntegerField(default=0)
+    str_mod= models.IntegerField(default=0)
+    int_mod= models.IntegerField(default=0)
+    wis_mod= models.IntegerField(default=0)
+    con_mod= models.IntegerField(default=0)
+    speed= models.IntegerField()
+    
