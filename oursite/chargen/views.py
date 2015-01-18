5f6772f99f486be0270038b6814b3e085e77fe06
davidricardo <davidr.ricardo@gmail.com>
2015-01-18 14:25:27 -0500

    Ability score calculator now works!
    It's not passing scores to python, but I'm working on that now.

--- oursite/chargen/views.py
+++ oursite/chargen/views.py
@@ -9,14 +9,150 @@ or at https://docs.djangoproject.com/en/1.6/topics/http/views/
 """
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, RequestContext, loader
+from django import forms
 
-from character import VARS_TO_PASS
+class AbilityScoreForm(forms.Form):
+    strength     = forms.ChoiceField(
+        [
+            (8, 8),
+            (9, 9),
+            (10, 10),
+            (11, 11),
+            (12, 12),
+            (13, 13),
+            (14, 14),
+            (15, 15),
+            (16, 16),
+            (17, 17),
+            (18, 18)
+            ],
+        widget = forms.Select(attrs = {
+            "onChange": 'updateAbilityScores()'
+            })
+        )
+    dexterity    = forms.ChoiceField(
+        [
+            (8, 8),
+            (9, 9),
+            (10, 10),
+            (11, 11),
+            (12, 12),
+            (13, 13),
+            (14, 14),
+            (15, 15),
+            (16, 16),
+            (17, 17),
+            (18, 18)
+        ],
+        widget = forms.Select(attrs = {
+            "onChange": 'updateAbilityScores()'
+            })
+        )
+    constitution = forms.ChoiceField(
+        [
+            (8, 8),
+            (9, 9),
+            (10, 10),
+            (11, 11),
+            (12, 12),
+            (13, 13),
+            (14, 14),
+            (15, 15),
+            (16, 16),
+            (17, 17),
+            (18, 18)
+        ],
+        widget = forms.Select(attrs = {
+            "onChange": 'updateAbilityScores()'
+            })
+        )
+    intelligence = forms.ChoiceField(
+        [
+            (8, 8),
+            (9, 9),
+            (10, 10),
+            (11, 11),
+            (12, 12),
+            (13, 13),
+            (14, 14),
+            (15, 15),
+            (16, 16),
+            (17, 17),
+            (18, 18)
+        ],
+        widget = forms.Select(attrs = {
+            "onChange": 'updateAbilityScores()'
+            })
+        )
+    wisdom       = forms.ChoiceField(
+        [
+            (8, 8),
+            (9, 9),
+            (10, 10),
+            (11, 11),
+            (12, 12),
+            (13, 13),
+            (14, 14),
+            (15, 15),
+            (16, 16),
+            (17, 17),
+            (18, 18)
+        ],
+        widget = forms.Select(attrs = {
+            "onChange": 'updateAbilityScores()'
+            })
+        )
+    charisma     = forms.ChoiceField(
+        [
+            (8, 8),
+            (9, 9),
+            (10, 10),
+            (11, 11),
+            (12, 12),
+            (13, 13),
+            (14, 14),
+            (15, 15),
+            (16, 16),
+            (17, 17),
+            (18, 18)
+        ],
+        widget = forms.Select(attrs = {
+            "onChange": 'updateAbilityScores()'
+            })
+        )
 
 def index(request):
-    #the template to load
     template = loader.get_template('chargen/index.html')
-    #contexts are a set of variable => value pairs that a template uses to generate a page.
-    context = Context(VARS_TO_PASS)
-    return HttpResponse(template.render(context))
+
+    if request.method == 'POST': # If the form has been submitted...
+        ab_score_form = AbilityScoreForm(request.POST) # A form bound to the POST data
+
+        if ab_score_form.is_valid(): # All validation rules pass
+            strength = ab_score_form.cleaned_data["strength"]
+            dexterity = ab_score_form.cleaned_data["dexterity"]
+            constitution = ab_score_form.cleaned_data["constitution"]
+            intelligence = ab_score_form.cleaned_data["intelligence"]
+            wisdom = ab_score_form.cleaned_data["wisdom"]
+            charisma = ab_score_form.cleaned_data["charisma"]
+            return HttpResponse(
+                "<p> You have submitted the form. Your ability scores are:</p>" + 
+                "<p>Strength: " + str(int(strength)) + "</p>" +
+                "<p>Dexterity: " + str(int(dexterity)) + "</p>" +
+                "<p>Constitution: " + str(int(constitution)) + "</p>" +
+                "<p>Intelligence: " + str(int(intelligence)) + "</p>" +
+                "<p>Wisdom: " + str(int(wisdom)) + "</p>" +
+                "<p>Charisma: " + str(int(charisma)) + "</p>"
+                )
+    
+    else:
+        ab_score_form = AbilityScoreForm() # An unbound form
+
+    return HttpResponse(
+            template.render(
+                RequestContext( request, {
+                'ab_score_form': ab_score_form
+                } )
+            )
+        )
